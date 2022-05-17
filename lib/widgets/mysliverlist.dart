7b8cea2a31521c68ba6101a5b329f7e5aad8c837
Jean-Robert <jean-robert.crenn@epitech.eu>
2022-05-17 08:15:01 +0200

    Fix padding data

--- lib/widgets/mysliverlist.dart
+++ lib/widgets/mysliverlist.dart
@@ -28,28 +28,37 @@ class MySliverList extends StatelessWidget {
                         height: 80,
                       ),
                     ),
-                    Text(
-                      names[math.Random().nextInt(names.length - 1)],
-                      style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 10.0,
-                          fontWeight: FontWeight.bold),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 42),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              names[math.Random().nextInt(names.length - 1)],
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 10.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              number[math.Random().nextInt(number.length - 1)] +
+                                  "€",
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 10.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              number[math.Random().nextInt(number.length - 1)] +
+                                  "%",
+                              style: TextStyle(
+                                  color: predefinedColors[math.Random()
+                                      .nextInt(predefinedColors.length)],
+                                  fontSize: 10.0,
+                                  fontWeight: FontWeight.bold),
+                            )
+                          ]),
                     ),
-                    Text(
-                      number[math.Random().nextInt(number.length - 1)] + "€",
-                      style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 10.0,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      number[math.Random().nextInt(number.length - 1)] + "%",
-                      style: TextStyle(
-                          color: predefinedColors[
-                              math.Random().nextInt(predefinedColors.length)],
-                          fontSize: 10.0,
-                          fontWeight: FontWeight.bold),
-                    )
                   ],
                 );
               })),
